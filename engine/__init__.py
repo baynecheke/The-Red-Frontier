7a4aea3aee83d85fc37fabc555d4ccd60b502249
baynecheke <51100443+baynecheke@users.noreply.github.com>
2026-02-12 09:45:35 -0500

    connect files

---
+++ engine/__init__.py
@@ -0,0 +1,3 @@
+from . import physics
+from . import life_support
+from . import crew
--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import json
 
-# --- CONNECTING THE MODULES ---
-# From the 'engine' folder, import the 'physics' file
-import engine.physics as physics 
-# From the 'engine' folder, import the 'life_support' file
-import engine.life_support as life_support
+import engine
+
+
 
 # 1. SETUP PHASE
 print("Initializing Simulation...")
@@ -12,7 +10,7 @@ ship_mass = 50000 # kg
 fuel = 120000 # kg
 
 # Use the physics module
-delta_v = physics.calculate_thrust(ship_mass, fuel)
+delta_v = engine.physics.calculate_thrust(ship_mass, fuel)
 print(f"Potential Delta-V: {delta_v:.2f} m/s")
 
 # 2. GAME LOOP (The Trip)
@@ -23,7 +21,7 @@ for day in range(1, 10): # Simulate 10 days
     print(f"--- Day {day} ---")
     
     # Use the life_support module
-    food_stock, status = life_support.consume_resources(crew_size, food_stock)
+    food_stock, status = engine.life_support.consume_resources(crew_size, food_stock)
     
     if status == "STARVATION":
         print("Mission Failed: Crew died.")
